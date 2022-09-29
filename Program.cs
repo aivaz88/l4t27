20f69181f9d58ccb9d6d6197a9106c5d8610be6b
aivaz88 <aivaz1988@mail.ru>
2022-09-29 12:57:43 +0500

    Calculating digital sum

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите любое целое число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{digital_sum(num)}");
+
+int digital_sum(int x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+        sum  += x % 10;
+        x = x / 10;
+    }
+    return(sum);
+}
